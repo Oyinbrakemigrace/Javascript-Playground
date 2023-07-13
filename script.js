04d70eed8b524a99c67a0874b169a33bb94c6dc4
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-07-14 01:30:24 +0400

    fizz buzz game

---
+++ script.js
@@ -0,0 +1,13 @@
+// Fizz Buzz Game
+
+for(let i = 0; i<=100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i + ' fizz buzz')
+    }else if(i%5===0){
+        console.log(i +' buzz')
+    }else if(i%3===0){
+        console.log(i+' fizz')
+    }else{
+        console.log(i + ' not divisible by 3 and 5')
+    }
+}
