c93129aa815eadf88923624d478424f77ab5eee4
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-07-26 13:02:18 +0400

    solving js questions

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Fizz Buzz Game
-
 // for(let i = 0; i<=100; i++){
 //     if(i % 3 === 0 && i % 5 === 0){
 //         console.log(i + ' fizz buzz')
@@ -13,6 +12,25 @@
 // }
 
 // function to print contents in the current window
-const printWindow = ()=>{
-    window.print()
-}
+// const printWindow = ()=>{
+//     window.print()
+// }
+
+// Get the current date in either of this format mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy .  
+// const today = new Date()
+// const day = today.getDate()
+// const month = today.getMonth() +1
+// const year = today.getFullYear()
+
+// if(day<10){
+//     var dd = `0${day}`
+// }else{
+//     var dd = day
+// }
+// if(month<10){
+//     var mm = `0${month}`
+// }else{
+//     var mm = month
+// }
+// const currentDate = `${dd}/${mm}/${year}`
+// console.log(currentDate)
