f6404e5a6ca39b0459e4fd081ca32aaccc544721
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-07-26 12:29:46 +0400

    solving js questions

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // Fizz Buzz Game
 
-for(let i = 0; i<=100; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-        console.log(i + ' fizz buzz')
-    }else if(i%5===0){
-        console.log(i +' buzz')
-    }else if(i%3===0){
-        console.log(i+' fizz')
-    }else{
-        console.log(i + ' not divisible by 3 and 5')
-    }
+// for(let i = 0; i<=100; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(i + ' fizz buzz')
+//     }else if(i%5===0){
+//         console.log(i +' buzz')
+//     }else if(i%3===0){
+//         console.log(i+' fizz')
+//     }else{
+//         console.log(i + ' not divisible by 3 and 5')
+//     }
+// }
+
+// function to print contents in the current window
+const printWindow = ()=>{
+    window.print()
 }
